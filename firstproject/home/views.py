d3e1b8d2401978b92d43fa274ff43d43be1f90ec
SergeySimonyan <sergey.simonyan03@gmail.com>
2022-01-17 17:04:47 +0400

    changed

--- firstproject/home/views.py
+++ firstproject/home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Task
 
 def index(request):
-    return render(request, 'home/index.html')
+    tasks = Task.objects.all()
+    return render(request, 'home/index.html', {'title': 'Site`s home page', 'tasks': tasks})
 
 def about(request):
     return render(request, 'home/about.html')
